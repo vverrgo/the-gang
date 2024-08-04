0f24815d0fc5aebccf3429c23757313aa88bed44
vverrgo <alexemoryamaya@gmail.com>
2024-08-04 14:24:23 -0500

    endofclass

---
+++ script.js
@@ -0,0 +1,14 @@
+console.log('hey there')
+
+
+function showPopup () {
+	document.getElementById('popup')
+var popup = document.getElementById('popup')
+popup.classList.add('show')
+}
+
+function hidePopup () {
+	document.getElementById('popup')
+var popup = document.getElementById('popup')
+popup.classList.remove('show')
+}
